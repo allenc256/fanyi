75354580ad04736f8561ec2af06d4d7b0cc03c80
allenc256 <allenc256@yahoo.com>
2022-07-08 00:43:44 -0700

    add views

--- fanyi/practice/urls.py
+++ fanyi/practice/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'practice'
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
+    path('convo/<int:pk>/', views.ConvoView.as_view(), name='convo'),
+    path('random/', views.random, name='random'),
 ]
--- fanyi/practice/views.py
+++ fanyi/practice/views.py
@@ -1,4 +1,24 @@
-from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.views import generic
+from random import choice
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+from .models import Conversation, Sentence
+
+class IndexView(generic.ListView):
+    template_name = 'practice/index.html'
+    model = Conversation
+    paginate_by = 20
+
+class ConvoView(generic.DetailView):
+    template_name = 'practice/convo.html'
+    model = Conversation
+    context_object_name = 'convo'
+
+def random(request):
+    all_ids = Conversation.objects.values_list('pk', flat=True)
+    random_id = choice(all_ids)
+    return redirect(f'/practice/convo/{random_id}/')
+
+# def convo(request, convo_id):
+#     convo = get_object_or_404(Conversation, pk=convo_id)
+#     return render(request, 'practice/convo.html', {'convo': convo})
