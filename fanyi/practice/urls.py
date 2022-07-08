5f443b5cdd32d061fb40e5dd2fafba6b3716496b
allenc256 <allenc256@yahoo.com>
2022-07-08 11:32:49 -0700

    add basic navigation

--- fanyi/practice/urls.py
+++ fanyi/practice/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 app_name = 'practice'
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
     path('convo/<int:pk>/', views.ConvoView.as_view(), name='convo'),
-    path('random/', views.random, name='random'),
+    path('convo/first/', views.convo_first, name='convo_first'),
+    path('convo/last/', views.convo_last, name='convo_last'),
+    path('convo/random/', views.convo_random, name='convo_random'),
 ]
--- fanyi/practice/views.py
+++ fanyi/practice/views.py
@@ -2,23 +2,32 @@ from django.shortcuts import redirect
 from django.views import generic
 from random import choice
 
-from .models import Conversation, Sentence
-
-class IndexView(generic.ListView):
-    template_name = 'practice/index.html'
-    model = Conversation
-    paginate_by = 20
+from .models import Conversation
 
 class ConvoView(generic.DetailView):
     template_name = 'practice/convo.html'
     model = Conversation
     context_object_name = 'convo'
 
-def random(request):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        next = Conversation.objects.only('pk').filter(pk__gt=context['convo'].pk).order_by('pk').first()
+        if next:
+            context['next_convo_id'] = next.pk
+        prev = Conversation.objects.only('pk').filter(pk__lt=context['convo'].pk).order_by('-pk').first()
+        if prev:
+            context['prev_convo_id'] = prev.pk
+        return context
+
+def convo_random(request):
     all_ids = Conversation.objects.values_list('pk', flat=True)
     random_id = choice(all_ids)
     return redirect(f'/practice/convo/{random_id}/')
 
-# def convo(request, convo_id):
-#     convo = get_object_or_404(Conversation, pk=convo_id)
-#     return render(request, 'practice/convo.html', {'convo': convo})
+def convo_first(request):
+    first = Conversation.objects.order_by('pk').first()
+    return redirect(f'/practice/convo/{first.pk}/')
+
+def convo_last(request):
+    last = Conversation.objects.order_by('-pk').first()
+    return redirect(f'/practice/convo/{last.pk}/')
