37e3b1834f4ed4465c7d2817ea8964e566059e91
allenc256 <allenc256@yahoo.com>
2022-07-13 17:11:28 -0700

    navigation tweaks

--- fanyi/practice/views.py
+++ fanyi/practice/views.py
@@ -1,6 +1,7 @@
 from django.views import generic
 from django.utils import timezone
 from django.urls import reverse
+from django.db.models import Count, Q
 
 from .models import Transcript, Entry
 
@@ -9,9 +10,16 @@ class IndexView(generic.list.ListView):
     model = Transcript
     paginate_by = 20
 
+    def get_queryset(self):
+        return Transcript.objects.annotate(entry_count=Count('entry')) \
+            .annotate(note_count=Count('entry', filter=Q(entry__notes__isnull=False) & ~Q(entry__notes='')))
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['recent'] = Transcript.objects.filter(last_viewed__isnull=False).order_by('-last_viewed')[:10]
+        context['recent'] = Transcript.objects.annotate(entry_count=Count('entry')) \
+            .annotate(note_count=Count('entry', filter=Q(entry__notes__isnull=False) & ~Q(entry__notes=''))) \
+            .filter(last_viewed__isnull=False) \
+            .order_by('-last_viewed')[:10]
         return context
 
 class TranscriptView(generic.list.ListView):
@@ -29,6 +37,14 @@ class TranscriptView(generic.list.ListView):
         context = super().get_context_data(**kwargs)
         transcript = Transcript.objects.get(pk=self.kwargs['transcript_pk'])
         context['transcript'] = transcript
+        context['transcript_next'] = Transcript.objects.only('pk') \
+            .filter(pk__gt=self.kwargs['transcript_pk']) \
+            .order_by('pk') \
+            .first()
+        context['transcript_prev'] = Transcript.objects.only('pk') \
+            .filter(pk__lt=self.kwargs['transcript_pk']) \
+            .order_by('-pk') \
+            .first()
         context['last_viewed'] = transcript.last_viewed
         transcript.last_viewed = timezone.now()
         transcript.save()
